f86dd77c6c45f804461fa8699aec15aca65f358b
gustavolbs <gustavo.luiz.bispo.santos@gmail.com>
2023-04-04 15:30:04 -0300

    add actions

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,10 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use("/auth", authRoutes);
 app.use("/city", cityRoutes);
